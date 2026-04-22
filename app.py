68977984b919bc5f989a61ed17c5ea702f111efa
thienlu1234 <143720766+thienlu1234@users.noreply.github.com>
2026-04-22 10:43:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -175,7 +175,7 @@ def kpi_card(title, value):
 col_logo, col_title = st.columns([1, 6])
 
 with col_logo:
-    st.image("logo_bidv2.png", width=120)
+    st.image("logo_bidv2.png")
 
 with col_title:
     st.markdown(
